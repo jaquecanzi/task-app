9770d30324b0d1039db2ecae029443e75f1bfff8
Jaqueline Copello <jaque.canzi@gmail.com>
2022-12-06 14:56:23 +0000

    Creating schema tasks

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const dotenv = require('dotenv').config()
 const express = require("express")
 const app = express()
 const connectDB = require('./backend/config/connectDB')
-
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5002
 
-connectDB()
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.log(err)
+})
 
 /*---  ROUTES  ---*/
 
@@ -13,7 +23,33 @@ app.get("/", (req,res) => {
     res.send('Home Page')
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running in port ${PORT}`)
+/*---  CREAT A TASK  ---*/
+
+app.post("/api/tasks", async (req, res) => {
+    console.log(req.body)
+    res.send("Task Created")
 })
+
+//-------------------------  Another way to connect server  --------------------------
+
+/* const startServer = async() => {
+
+    try {
+       await connectDB()
+       app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`)
+       }) 
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer() */
+
+
+
+/* app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`)
+}) */
 
